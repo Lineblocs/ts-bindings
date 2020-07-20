a77878a490b3d020d6902031947c89b065408792
Nadir Hamid <matrix.nad@gmail.com>
2020-07-20 07:27:01 +0000

    add Dockerfile

--- server.js
+++ server.js
@@ -6,73 +6,87 @@ var fs = require("fs");
 var path = require("path");
 var tmp = require("tmp");
 const app = express()
-const port = 3000
+const port = 8080;
 var compilerTypings;
-var base = fs.readFileSync(__dirname+"/files/defs.ts");
+var pathToDefs = __dirname+"/files/defs.ts";
+var base = fs.readFileSync(pathToDefs);
+var lines;
 
 
 
 
 compilerTypings = base.toString('UTF-8');
 
-
+function countFileLines() {
+  return new Promise((resolve, reject) => {
+  let lineCount = 0;
+  fs.createReadStream(pathToDefs)
+    .on("data", (buffer) => {
+      let idx = -1;
+      lineCount--; // Because the loop will run once for idx=-1
+      do {
+        idx = buffer.indexOf(10, idx+1);
+        lineCount++;
+      } while (idx !== -1);
+    }).on("end", () => {
+      resolve(lineCount);
+    }).on("error", reject);
+  });
+};
 // parse application/json
 app.use(bodyParser.json())
 app.post('/compile', (req, res) => {
     var code = req.body.code;
-    var compile = compilerTypings + "\r\n" + code;
-    tmp.file(async function _tempFileCreated(err, path, fd, cleanupCallback) {
+    var compile = compilerTypings + code;
+    tmp.file({ template: 'tmp-XXXXXX.ts' },async function _tempFileCreated(err, path, fd, cleanupCallback) {
         if (err) throw err;
         
         console.log('File: ', path);
         console.log('Filedescriptor: ', fd);
-        var out = path + ".out";
+        var out = path.replace(/\.ts/, ".out.ts");
         try {
             await fs.promises.appendFile(path, compile);
         } catch (err) {
-            res.statusCode(500);
+            res.status(500);
             res.json({"error": "Internal"});
             return;
         }
         try {
             var log = await tsc.compile({
                 outFile: out
-            }, file, {verbose: true})
+            }, path, {verbose: true})
             console.log("result ", log);
-            var result = await fs.promises.readFile(loc);
+            var result = await fs.promises.readFile(out);
             res.json({"compiled": result.toString('UTF-8')})  ;
 
         } catch (err) {
-            res.statusCode(500);
-            res.json({"error": "Compile", "code": err});
+		console.log("PROGRAM ERRORS: ");
+		console.error( err );
+		var logs = err.stdout.split("\n").map((line) => {
+			var splitted = line.split(":");
+			var first = splitted[0];
+			var errorLineInfo = first.match(/\(([0-9]+),([0-9]+)\)$/);	
+			var line = parseInt( errorLineInfo[1] );
+			//subtract the definitions from the line count as the user doesnt see those in the frontend
+			line = line - lines;
+			var character = parseInt( errorLineInfo[2] );
+			var joined = splitted.slice(1, splitted.length).join(":");
+			return {
+				debug: joined,
+				line,
+				character
+			};
+		});
+            res.status(500);
+            res.json({"error": "Compile", "log": logs});
             return;
         }
         cleanupCallback();
     });
-
-    tsc.compile({
-        //'project': '.'
-        outFile: loc
-    }, file, {verbose: true})
-    .then(function(log) {
-        console.log("result ", log);
-        console.log(fs.readFileSync(loc).toString('UTF-8'));
-    }, function(err) {
-        console.log(err);
-    });
-
 });
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-tmp.file(async function _tempFileCreated(err, path, fd, cleanupCallback) {
-  if (err) throw err;
- 
-  console.log('File: ', path);
-  console.log('Filedescriptor: ', fd);
-  await fs.promises.appendFile(path, "test");
-  console.log((await fs.promises.readFile(path)).toString('UTF-8'));
+setImmediate(async function() {
+	lines = await countFileLines();
+	console.log("lines ", lines);
 
-  // If we don't need the file anymore we could manually call the cleanupCallback
-  // But that is not necessary if we didn't pass the keep option because the library
-  // will clean after itself.
-  cleanupCallback();
+	app.listen(port, "0.0.0.0", () => console.log(`Example app listening at http://localhost:${port}`))
 });
